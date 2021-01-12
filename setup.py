ab4dc07e8c8f57b09822b579c52f2f2756e169b8
kevin ju <kevinju0827@gmail.com>
2021-01-12 11:19:47 +0800

    Add package and setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyscap',
+    version='0.1.0',
+    packages=['pyscap'],
+    url='https://github.com/Little-Tetra/pyscap',
+    license='MIT',
+    author='kevinju',
+    author_email='',
+    description='Python package for handling Security Content Automation Protocol.'
+)
