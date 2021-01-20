305a8b95647be879d297cd72b78033760f62a8d7
kevin ju <kevinju0827@gmail.com>
2021-01-20 16:26:37 +0800

    Add CPE constants.

---
+++ pyscap/specs/cpe/consts.py
@@ -0,0 +1,2 @@
+CPE2_NAMESPACE = "http://cpe.mitre.org/language/2.0"  # Common Platform Enumeration (CPE) 2.3 Applicability Language
+CPE2_DICT_NAMESPACE = "http://cpe.mitre.org/dictionary/2.0"  # CPE 2.3 Dictionary
