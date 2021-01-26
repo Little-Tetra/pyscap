e31c0e4dd1b026e3cf088c0d0a3a2d8f249f2c4b
kevin ju <kevinju0827@gmail.com>
2021-01-26 18:05:56 +0800

    Update version to 0.1.1.

---
+++ CHANGES.rst
@@ -0,0 +1,13 @@
+Version 0.1.1
+-------------
+
+Released 2021-01-26
+
+- Implement XCCDF and OVAL classes, load, dump functions.
+
+Version 0.1.0
+-------------
+
+Released 2021-01-19
+
+- Initialize the project
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyscap',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     url='https://github.com/Little-Tetra/pyscap',
     license='MIT',
